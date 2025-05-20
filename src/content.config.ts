4ac6caf7b72173f97eed294390891ee5cb31bec3
Chen Zhao <czhao.cz44@gmail.com>
2025-05-19 20:56:16 -0400

    publication selection

--- src/content.config.ts
+++ src/content.config.ts
@@ -53,6 +53,7 @@ const publication = defineCollection({
 			.string()
 			.date() // Ensures ISO 8601 format with offsets allowed (e.g. "2024-01-01T00:00:00Z" and "2024-01-01T00:00:00+02:00")
 			.transform((val) => new Date(val)),
+		isSelected: z.boolean().default(false),		
 		authors: z.string().optional(),
 		moreAuthors: z.string().optional(),
 		correspondingAuthors: z.string().optional(),
